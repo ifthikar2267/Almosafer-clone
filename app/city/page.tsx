d906bb80cdc072ca1fefb69ee3b16f6140413e95
ifthikar2267 <seyedahamedifthikar40@gmail.com>
2025-08-29 22:39:37 +0530

    font

--- app/city/page.tsx
+++ app/city/page.tsx
@@ -1,14 +1,5 @@
-// app/city/page.tsx
-import { Suspense } from "react";
-import dynamic from "next/dynamic";
-
-const City = dynamic(() => import("./city"), { ssr: false });
-
+import City from "./city";
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading city...</div>}>
-      <City />
-    </Suspense>
-  );
+  return <City />;
 }
