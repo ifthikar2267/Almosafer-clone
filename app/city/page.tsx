1a4472e4ae75f0308a9ac69c8ad282224432c7d2
ifthikar2267 <seyedahamedifthikar40@gmail.com>
2025-08-29 23:39:13 +0530

    font

--- app/city/page.tsx
+++ app/city/page.tsx
@@ -1,6 +1,11 @@
-
+// app/city/page.tsx
+import { Suspense } from "react";
 import City from "./city";
 
 export default function Page({ searchParams }: { searchParams: { [key: string]: string } }) {
-  return <City city={searchParams.city || ""} />;
+  return (
+    <Suspense fallback={<div>Loading city...</div>}>
+      <City city={searchParams.city || ""} />
+    </Suspense>
+  );
 }
